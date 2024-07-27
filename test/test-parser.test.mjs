c75cf61bee4dc0d6b81e41582800f6bbb7c49464
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-27 17:47:57 +1000

    read the forecast data

--- test/test-parser.test.mjs
+++ test/test-parser.test.mjs
@@ -10,8 +10,21 @@ const __dirname = path.dirname(__filename)
 const daylesfordData = (await fs.readFile(path.join(__dirname, 'mock', 'daylesford.html'))).toString()
 
 describe('The parser', () => {
+  let data = parse(daylesfordData)
+
   it('Should extract the temperature of the nearest weather station', () => {
-    let data = parse(daylesfordData)
     expect(data.currentTemperature).to.equal(3.5)
+  })
+
+  it('Should return the forecast for the next few days', () => {
+    expect(data.forecasts).to.deep.equal([
+      { day: "2024-07-27", forecast: 'Showers' },
+      { day: "2024-07-28", forecast: 'Showers' },
+      { day: "2024-07-29", forecast: 'Showers' },
+      { day: "2024-07-30", forecast: 'Partly Cloudy' },
+      { day: "2024-07-31", forecast: 'Partly Cloudy' },
+      { day: "2024-08-01", forecast: 'Partly Cloudy' },
+      { day: "2024-08-02", forecast: 'Partly Cloudy' }
+    ])
   })
 })
